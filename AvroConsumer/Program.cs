50ed3f825439d59ff306cedfc5249463e11cd0db
angela-tolentino <tolentino.angela.a@gmail.com>
2025-03-15 22:12:22 -0700

    first commit

---
+++ AvroConsumer/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using Avro.Generic;
+using Npgsql;
+using Confluent.Kafka.SyncOverAsync;
+class Program 
+{
+    private const string KafkaBootstrapServers = "kafka:9092";
+    private const string SchemaRegistryUrl = "http://schema-registry:8081";
+      private const string UserTopicName = "avro_topic_user";
+        private const string OrderTopicName = "avro_topic_order";
+
+    private const string PostgresConnectionString = "Host=postgres;Username=postgres;Password=postgres;Database=avrodb";
+
+    static async Task Main() 
+    {
+        var schemaRegistryConfig = new SchemaRegistryConfig { Url = SchemaRegistryUrl };
+        using var schemaRegistry = new CachedSchemaRegistryClient(schemaRegistryConfig);
+         var config = new ConsumerConfig 
+         {
+            BootstrapServers = KafkaBootstrapServers,
+            GroupId = "avro_group",
+            AutoOffsetReset = AutoOffsetReset.Earliest,
+            EnableAutoCommit = true
+         };
+
+         using var consumer = new ConsumerBuilder<string, GenericRecord>(config)
+         .SetValueDeserializer(new AvroDeserializer<GenericRecord>(schemaRegistry).AsSyncOverAsync())
+         .Build();
+
+        consumer.Subscribe(new[] { UserTopicName, OrderTopicName });
+
+         using var connection = new NpgsqlConnection(PostgresConnectionString);
+         await connection.OpenAsync();
+
+         while (true)
+         {
+            var result = consumer.Consume(CancellationToken.None);
+            var schema = result.Message.Value.Schema;
+            var tableName = schema.Name.ToLower();
+
+            var createTableSQL = new StringBuilder($"CREATE TABLE IF NOT EXISTS {tableName} (");
+            foreach(var field in ((Avro.RecordSchema)schema).Fields)
+            {
+                createTableSQL.Append($"{field.Name} {GetPostgresType(field.Schema)}, ");
+            }
+            createTableSQL.Length -= 2;
+            createTableSQL.Append(");");
+            Console.WriteLine(createTableSQL);
+
+            await using var createTableCmd = new NpgsqlCommand(createTableSQL.ToString(), connection);
+            await createTableCmd.ExecuteNonQueryAsync();
+
+            var insertSQL = new StringBuilder($"INSERT INTO \"{tableName}\" (");
+            var valueSQL = new StringBuilder(" VALUES (");
+            foreach (var field in schema.Fields)
+            {
+                insertSQL.Append($"{field.Name}, ");
+                valueSQL.Append($"'{result.Message.Value[field.Name]}', ");
+            }
+            insertSQL.Length -= 2;
+            valueSQL.Length -= 2;
+            insertSQL.Append(")");
+            valueSQL.Append(");");
+            Console.WriteLine(insertSQL);
+            Console.WriteLine(valueSQL);
+
+            await using var transaction = await connection.BeginTransactionAsync();
+            await using var insertCmd = new NpgsqlCommand(insertSQL.ToString() + valueSQL.ToString(), connection);
+            await insertCmd.ExecuteNonQueryAsync();
+            await transaction.CommitAsync();
+
+            Console.WriteLine($"Stored record in {tableName}");
+         }
+    }
+
+    private static string GetPostgresType(Avro.Schema schema) => 
+        schema.Tag switch 
+        {
+            Avro.Schema.Type.Int => "INTEGER",
+            Avro.Schema.Type.String => "TEXT",
+            Avro.Schema.Type.Double => "DOUBLE PRECISION",
+            _ => "TEXT"
+        };
+}
---
+++ AvroProducer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using Avro.Generic;
+
+namespace AvroProducer 
+{
+    class Program 
+    {
+        private const string KafkaBootstrapServers = "kafka:9092";
+        private const string SchemaRegistryUrl = "http://schema-registry:8081";
+        private const string UserTopicName = "avro_topic_user";
+        private const string OrderTopicName = "avro_topic_order";
+
+
+        static  async Task Main()
+        {
+             var schemaRegistryConfig = new SchemaRegistryConfig { Url = SchemaRegistryUrl };
+            using var schemaRegistry = new CachedSchemaRegistryClient(schemaRegistryConfig);
+
+            var config = new ProducerConfig {BootstrapServers = KafkaBootstrapServers };
+
+            using var producer = new ProducerBuilder<string, GenericRecord>(config)
+            .SetValueSerializer(new AvroSerializer<GenericRecord>(schemaRegistry))
+            .Build();
+
+            var schemas = new[] {
+                "{\"type\":\"record\",\"name\":\"Users\",\"fields\":[{\"name\":\"id\",\"type\":\"int\"},{\"name\":\"name\",\"type\":\"string\"}]}",
+                "{\"type\":\"record\",\"name\":\"Orders\",\"fields\":[{\"name\":\"order_id\",\"type\":\"int\"},{\"name\":\"amount\",\"type\":\"double\"}]}"
+            };
+
+            foreach(var schemaJson in schemas){
+                var schema = Avro.Schema.Parse(schemaJson);
+                var record = new GenericRecord((Avro.RecordSchema)schema);
+
+                if (schema.Name == "Users"){
+                    record.Add("id",1);
+                    record.Add("name", "John Doe");
+                    await producer.ProduceAsync(UserTopicName, new Message<string, GenericRecord> { Key = schema.Name, Value = record });
+
+                } else {
+                    record.Add("order_id", 1001);
+                    record.Add("amount", 250.75);
+                    await producer.ProduceAsync(OrderTopicName, new Message<string, GenericRecord> { Key = schema.Name, Value = record });
+
+                }
+                Console.WriteLine($"Produced message with schema: {schema.Name}");
+            }
+        }
+    }
+}
